4ecf1165de681d71716e14858a32f2f88ace05e0
Jose Ayerdis <joseayerdis@gmail.com>
2013-10-19 00:01:04 -0600

    Hide keyboard programatically

--- src/com/atech/mpso/MPsoActivity.java
+++ src/com/atech/mpso/MPsoActivity.java
@@ -1,72 +1,79 @@
 package com.atech.mpso;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MPsoActivity extends Activity implements ResponseCallback{
+public class MPsoActivity extends Activity implements ResponseCallback {
 
 	private EditText editText;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setTitle(R.string.title);
 		setContentView(R.layout.activity_mpso);
-		
-		editText = (EditText)findViewById(R.id.editTUC);
-		
+
+		editText = (EditText) findViewById(R.id.editTUC);
+
 		editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-	        @Override
-	        public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-	            if (actionId == EditorInfo.IME_ACTION_DONE) {
-	            		search();
-	                return true;
-	            }
-	            return false;
-	        }
-	    });
-		
+			@Override
+			public boolean onEditorAction(TextView v, int actionId,
+					KeyEvent event) {
+				if (actionId == EditorInfo.IME_ACTION_DONE) {
+					search();
+					InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+					imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+					return true;
+				}
+				return false;
+			}
+		});
+
 		findViewById(R.id.search).setOnClickListener(new OnClickListener() {
-			
+
 			@Override
 			public void onClick(View v) {
 				search();
 			}
 		});
-		
-		if (BuildConfig.DEBUG) 
+
+		if (BuildConfig.DEBUG)
 			editText.setText("00442039");
 	}
-	
+
 	private void search() {
 		String tarjetaTUC = editText.getText().toString();
 		if (valid(tarjetaTUC))
-			new MPesoCaller(getBaseContext()).consultarSaldo(tarjetaTUC, MPsoActivity.this);
+			new MPesoCaller(getBaseContext()).consultarSaldo(tarjetaTUC,
+					MPsoActivity.this);
 		else
 			editText.setError(getString(R.string.validation_error));
 	}
-	
+
 	private boolean valid(String tarjetaTUC) {
-		
+
 		try {
 			Integer.parseInt(tarjetaTUC);
-		} catch(NumberFormatException ex) {
+		} catch (NumberFormatException ex) {
 			return false;
 		}
-		
+
 		return tarjetaTUC.length() == 8;
 	}
 
 	@Override
 	public void response(String saldo) {
-		Toast.makeText(this, "saldo is " + saldo, Toast.LENGTH_LONG).show();;
+		Toast.makeText(this, "saldo is " + saldo, Toast.LENGTH_LONG).show();
+		;
 	}
 
 	@Override
